67f1ea273129882e556c3fc6eef3499ce31e998b
torrado <pedro.espanha.torrado@gmail.com>
2025-08-11 22:07:26 +0100

    Initial Commit

---
+++ Notepad/NotepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotepadApp.swift
+//  Notepad
+//
+//  Created by Pedro Torrado on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotepadTests/NotepadTests.swift
@@ -0,0 +1,16 @@
+//
+//  NotepadTests.swift
+//  NotepadTests
+//
+//  Created by Pedro Torrado on 11/08/2025.
+//
+
+import Testing
+
+struct NotepadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
